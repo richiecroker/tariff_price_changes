c0ab3eb1636ad3cc78dba3f006cf90341ffc6c45
Richard Croker <ra.croker@gmail.com>
2026-01-22 21:29:50 +0000

    add rx label

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,7 @@ def gbp2f(x):
 st.header("Drug Tariff price change estimator", divider ="blue")
 
 st.markdown(f"### Drug Tariff month: {max_tariff_date}")
+st.markdown(f"### Prescribing data used for estimate: {max_rx_date}")
 
 names = ["(All)"] + sorted(icb_df["name"].dropna().unique().tolist())
 st.markdown("### Select Integrated Care Board")
