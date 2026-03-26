2fe37ed5f4ccd40e8dbf00499091005408907e13
Richard Croker <ra.croker@gmail.com>
2026-03-26 22:43:46 +0000

    add dates

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,11 +80,14 @@ vmpp_df = conn.execute("""
 #vmpp_df = pd.DataFrame(vmpp_data)
 
 # Get latest dates
-#raw_max_rx_date = data_loader.get_cached_max_rxdate()
+
 #max_rx_date = pd.to_datetime(raw_max_rx_date, errors="coerce").strftime("%B %Y")
+max_rx_date = conn.execute("SELECT MAX(month) FROM prescribing").fetchone()[0]
+
 
 #raw_max_tariff_date = data_loader.get_cached_max_tariffdate()
 #max_tariff_date = pd.to_datetime(raw_max_tariff_date, errors="coerce").strftime("%B %Y")
+max_tariff = conn.execute("SELECT MAX(date) FROM tariff_price_changes").fetchone()[0]
 
 # calculate number of changes to vmpp
 
