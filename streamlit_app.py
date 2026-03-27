85acde5c90398db97a3bb13677e0644f49c237f3
Richard Croker <ra.croker@gmail.com>
2026-03-27 21:48:28 +0000

    fix dates problem

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,6 +122,9 @@ practices_df = load_practices_df(conn)
 tariff_cat_opts = load_tariff_categories(conn)
 
 dates = get_latest_dates()
+st.write("DEBUG dates:", dates)
+conn2 = get_duckdb_connection()
+st.write("DEBUG tables:", conn2.execute("SHOW TABLES").fetchall())
 max_rx_date = dates["prescribing"]
 max_tariff_date = dates["tariff"]
 
