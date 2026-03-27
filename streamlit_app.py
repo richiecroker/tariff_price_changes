4db1dcc774e303ad3d430f458fec82bda0eaf00a
Richard Croker <ra.croker@gmail.com>
2026-03-27 14:48:34 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -219,7 +219,7 @@ with col_next:
 
 st.divider()
 
-with st.expander("Click to see our methodology", icon="ℹ️"):
+with st.expander("Click to see our methodology", icon=":material/quick_reference:"):
     st.write('''
         we're great!
         ''')
